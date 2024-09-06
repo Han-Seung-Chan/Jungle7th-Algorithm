1f47f0f3390bddf372ce203e52347d99acfe5499
seungchan Han <93566353+Han-Seung-Chan@users.noreply.github.com>
2024-09-07 01:34:16 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,27 @@
+import sys
+import math
+
+sint = sys.stdin.readline
+
+c = int(sint())
+data = sint().split()
+
+def isPrime(x):
+    if x == 1:
+        return False
+
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+
+    return True
+
+
+result = 0
+
+for i in data:
+
+    if isPrime(int(i)) == True:
+        result += 1
+
+print(result)
